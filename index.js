59af1893f504d2447870223b15115eeed99e0fbc
RaiyanKhalil <raiyankhalil@gmail.com>
2019-08-15 23:24:37 +0600

    ASSIGNMENT-2

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+
+app.get('/home', function(req, res){
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/pageone', function(req, res) {
+  res.render('pageone.ejs');
 });
 
 
 
+io.sockets.on('connection', function(socket) {
+  socket.on('username', function(username) {
+    socket.username = username;
 
-  io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
-  });
-  
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });                
+
+    io.emit('is_online', socket.username + ' join the chat..</i>');
+
+  })
+})
 
 http.listen(process.env.PORT || 3000 , function(){
   console.log('listening on *:3000');
